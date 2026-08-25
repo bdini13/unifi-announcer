93885c884d1daa3ce79efe6f176878227527d6cb
Bobby Dini <109565994+bdini13@users.noreply.github.com>
2026-08-25 07:17:51 -0400

    fix(ha): align beta2 integration constants

--- custom_components/unifi_announcer/const.py
+++ custom_components/unifi_announcer/const.py
@@ -1,5 +1,6 @@
 """Constants for the UniFi Announcer Home Assistant integration."""
 DOMAIN = "unifi_announcer"
+INTEGRATION_VERSION = "2.1.0-beta.2"
 PLATFORMS = ["notify", "button", "select", "sensor", "media_player"]
 CONF_URL = "url"
 CONF_API_KEY = "api_key"
